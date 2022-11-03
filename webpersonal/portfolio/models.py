bfcd88f730e847401685f7c2344202478fb7d34a
miguel-la-rocca <ing.miguellarocca@gmail.com>
2022-11-03 17:31:06 -0300

    add portfolio &  admin

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="Dirección Web", null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+
